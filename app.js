8ce9769a9e37faf94d5417398f55b4cc0992744d
Kiwied <anik-anton@yandex.ru>
2020-10-25 15:35:45 +0300

    refactor: rateLimiter middleware moved to utils/

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const rateLimit = require('express-rate-limit');
+
 const { celebrate, Joi, errors } = require('celebrate');
 const helmet = require('helmet');
 
@@ -12,6 +12,7 @@ const auth = require('./middlewares/auth');
 const { signin, signup } = require('./controllers/users');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const centralizedErrorHandler = require('./middlewares/centralizedErrorHandler');
+const limiter = require('./utils/rateLimiter');
 
 const { NODE_ENV, MONGODB, PORT = 3000 } = process.env;
 const app = express();
@@ -23,11 +24,6 @@ mongoose.connect(NODE_ENV === 'production'
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
-});
-
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
 });
 
 app.use(limiter);
